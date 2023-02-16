85925c64e7a8a1e004efbfcaeb1a0caf2349c4b9
Orynik <dkrovel@gmail.com>
2023-02-16 19:24:56 +0800

    feat: add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+export default {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/vue3-essential'
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'vue'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
