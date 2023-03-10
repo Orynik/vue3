1ca458253e3116d41bfb938c8db41f10b222d51a
Orynik <dkrovel@gmail.com>
2023-03-10 23:28:09 +0800

    Add formatters and static analytics

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,32 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true,
+    browser: true,
   },
+  plugins: ['prettier'],
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-unused-components": 2,
-    "vue/custom-event-name-casing": "off",
-    "vue/multi-word-component-names": 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-components': 2,
+    'vue/custom-event-name-casing': 'off',
+    'vue/multi-word-component-names': 0,
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
-      env: {
-        jest: true,
-      },
     },
   ],
 };
